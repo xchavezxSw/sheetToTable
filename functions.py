46c91e7bb5ba2b5d9a32d734f9d737e80f1bf736
xchavezxSw <claudio.x.pc@gmail.com>
2022-08-18 14:21:00 -0300

    xxxx
    xxxxx

---
+++ functions.py
@@ -0,0 +1,12 @@
+# all functions imported
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+def jsonsheet():
+    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('ultimo.json', scope)
+    client = gspread.authorize(credentials)
+    sheet4 = client.open('[EnProceso]EnCliente').worksheet('EnProcesoEnCliente')  # Open the spreadsheet
+    data=sheet4.get_all_records()
+    return json.dumps(data)
