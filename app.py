2517bc8ec0e40cd2db934121af831650220d7e50
Your Name <claudio.x.pc@gmail.com>
2022-08-24 11:21:24 -0300

    ci
    OD# with '#' will be ignored, and an empty message aborts the commit.

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
+import os
 from flask import Flask
-from functions import jsonsheet
+from functions import jsonsheet,addReserva,jsonReservas
 app = Flask(__name__)
 import json
-from flask import jsonify, make_response
+from flask import jsonify,request, make_response
+from flask_cors import CORS
+CORS(app)
 @app.route('/')
 def hello():
     data=jsonsheet()
     print(data)
     return make_response(jsonify(data), 200)
+
+@app.route('/insert', methods=['GET', 'POST'])
+def parse_request():
+    if request.method == 'POST':
+        value={  "email":request.form.get('email'),
+                "emailCandidato":request.form.get('emailCandidato'),
+                "naCandi":request.form.get('naCandi'),
+                "lkCandi":request.form.get('lkCandi'), 
+                "tcandi":request.form.getlist('tcandi[]'),
+                "tperfil":request.form.getlist('tperfil[]'),
+                "idReserva":request.form.get('idReserva'),
+                "comment":request.form.get('comment')
+                }
+        addReserva(value)
+    return 'ok', 200 
+
+@app.route('/reservas')
+def reserva():
+    data=jsonReservas()
+    return make_response(jsonify(data), 200)
+@app.route('/quit')
+def _quit():
+    os._exit(0)
+
--- functions.py
+++ functions.py
@@ -1,12 +1,43 @@
 # all functions imported
+from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
-def jsonsheet():
-    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
              "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('ultimo.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('ultimo.json', scope)
+client = gspread.authorize(credentials)
+reservas = client.open('[AUT]PedidosReservas').worksheet('PedidosReservas')  # Open the spreadsheet
+reservado=client.open('[EnProceso]EnConexionReservado').worksheet('EnProcesoEnConexionReservado')
+def jsonsheet():
     client = gspread.authorize(credentials)
     sheet4 = client.open('[EnProceso]EnCliente').worksheet('EnProcesoEnCliente')  # Open the spreadsheet
     data=sheet4.get_all_records()
-    return json.dumps(data)
+    nuevo=[]
+    for i in data:
+        i['url']='<button>clau</button>'
+        nuevo.append(i)
+    return json.loads(json.dumps(nuevo).encode('utf-8').decode('ascii'))
+
+def addReserva(values):
+    curDT = datetime.now()
+    date_time = curDT.strftime("%m/%d/%Y, %H:%M:%S")
+    email=values['email']
+    emailCandidato=values['emailCandidato']
+    naCandi=values['naCandi']
+    lkCandi=values['lkCandi']
+    tecnologias=",".join(values['tcandi'])
+    tcandi=tecnologias
+    tipoPerfil=",".join(values['tperfil'])
+    tperfil=tipoPerfil
+    idReserva=values['idReserva']
+    comment=values['comment']
+    reservas.append_row([date_time,email,emailCandidato,naCandi,lkCandi,tcandi,tperfil,idReserva,comment])
+
+def jsonReservas():
+    data=reservado.get_all_records()
+    nuevo=[]
+    for i in data:
+        i['url']='<button>edit</button>'
+        nuevo.append(i)
+    return json.loads(json.dumps(nuevo).encode('utf-8').decode('ascii'))
