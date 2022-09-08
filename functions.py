4f652a13a95bd6902fdeb26c9e0fb5b759a36eea
xchavezx <ccordoba@biometricpro.io>
2022-09-08 15:22:59 -0300

    updatre

--- functions.py
+++ functions.py
@@ -110,7 +110,7 @@ def getInformesArevisar():
           if i[4]!="Anclada":
             if '_' in i[9] :
                 for j in i[9].split("_"):
-                    i[9]=j
+                    i[9]=j.replace("ID","")
                     i.append("""<button style="border:none; background-color: transparent;" id="aprobar"><img width="30px" src="https://static.vecteezy.com/system/resources/previews/010/153/967/non_2x/tick-icon-accept-approve-sign-design-free-png.png" alt=""></button>
                         <button style="border:none; background-color: transparent;" id="rechazar"> <img width="30px"  src="https://geoinn.com/wp-content/uploads/2018/08/010_x-3-512.png" alt=""></button>""")
             else:
