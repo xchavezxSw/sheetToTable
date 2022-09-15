f0af29a713af275ec0cdeab73825e285f289902d
= <ccordoba@biometricpro.io>
2022-09-15 14:33:03 -0300

    se comitea

--- mysql.py
+++ mysql.py
@@ -17,10 +17,10 @@ def reservados():
         a = conexion.cursor()
         consulta = "select * from reserva;"
         a.execute(consulta)
-        row_headers = [x[0] for x in a.description]  # this will extract row headers
+        #row_headers = [x[0] for x in a.description]  # this will extract row headers
         results = a.fetchall()
         json_data = []
-        json_data.append(row_headers)
+       # json_data.append(row_headers)
         for result in results:
             json_data.append( [result[0],result[1],result[2],result[3],result[4],result[5],
                                                     result[6],result[7],result[8],result[9],result[10],str(result[11])])
