dcce2d8fcc91ad3d432c5dbbf42eae8ce417cd43
= <ccordoba@biometricpro.io>
2022-09-15 11:43:48 -0300

    se comitea

--- mysql.py
+++ mysql.py
@@ -22,10 +22,9 @@ def reservados():
         results = a.fetchall()
         json_data = []
         for result in results:
-            json_data.append(dict(zip(row_headers, result)))
-
+            json_data.append(json.dumps(dict(zip(row_headers, [result[0],result[1],result[2],result[3],result[4],result[5],
+                                                    result[6],result[7],result[8],result[9],result[10],str(result[11])]))))
         return json_data
-
 def contratados():
     try:
         a = conexion.cursor()
@@ -60,3 +59,4 @@ motivo,status,ComentariosAdicionales,FECHA    ) VALUES (0, '"+values['email']+"'
         return json.loads(json.dumps(result).encode('utf-8').decode('ascii'))
 
 
+reservados()
