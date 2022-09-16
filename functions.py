428848309d698905f83d8e073ddb55a1941ac252
= <ccordoba@biometricpro.io>
2022-09-15 23:03:18 -0300

    se comitea

--- functions.py
+++ functions.py
@@ -166,6 +166,11 @@ def revisarRechaz(values):
     curDT = datetime.datetime.now()
     date_time = curDT.strftime("%m/%d/%Y, %H:%M:%S")
     eliminar_guiones(EMailCandidato, IdsaEnviar, Email)
+    for i in SolicitudInforme.get_all_values():
+        if i[6] == Email and i[8] == EMailCandidato and i[9] == IdsaEnviar:
+            indice = ind
+            SolicitudInforme.delete_row(ind)
+        ind=ind+1
     SolicitudInforme.append_row([True,"","" ,IdsaEnviar,StatusEnBase,date_time,Email,
     EsSource, EMailCandidato, IdsaEnviar, TecnoCandi, TpCandi, LKCandi, comment,
     CvEspañol, InfoEntrevista, CvIngles, InfoEntrevistaIngles, RemuneracionPretendidaMensual,
