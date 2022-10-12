d097c92ac82425b92eb77c74b71c3a2b82aa1d28
= <claudio.cordoba@99minutos.com>
2022-10-12 15:21:58 -0300

    se comitea

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from functions import *
 import json
 from flask_cors import CORS,cross_origin
 app = Flask(__name__)
-CORS(app)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_COOKIE_SAMESITE"] = "None"
