c946360dc47109de1af06d8c7de90b47f14f0540
= <claudio.cordoba@99minutos.com>
2022-10-18 13:08:09 -0300

    se comitea

--- bd.py
+++ bd.py
@@ -1,7 +1,8 @@
 import mysql as dd
-conexion= dd.connector.connect(
+conexion= dd.connect(
     host="127.0.0.1",
     user="root",
     password="Oracle50",
     database="conexion"
 )
+conexion.ping()
