4b0a65d370a3d1d40a1404e53227a4805f317db5
claudio <claudio.x.pc@gmail.com>
2022-12-27 23:48:11 -0300

    agg

--- app.py
+++ app.py
@@ -96,6 +96,7 @@ def base64():
         email = args.get('email')
         emailc = args.get('emailc')
     data=base64decode(id,email,emailc)
+    print(data)
     data = encode(data, 'utf-8')
     print(data)
     content =b64.decodebytes(data)
