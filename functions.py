d483ba47dd75878af548aff9ea98987f8b44b4cd
claudio <claudio.x.pc@gmail.com>
2023-01-03 14:35:39 -0300

    se agregan cambios

--- functions.py
+++ functions.py
@@ -525,7 +525,7 @@ def sendmailstatus(emailCandi,idSt,emailSt,statusSt,comentarios=""):
     client = gspread.authorize(credentials)
     sheet4 = client.open('sendMails').worksheet('mails')
     sheet4.add_rows(1)
-    sheet4.append_row([datetime.datetime.today().strftime('%Y-%m-%d %H:%M'),emailSt,emailCandi,idSt,status(statusSt),comentarios,"","","","","","","","","","",0],"A2:Q2")
+    sheet4.append_row([datetime.datetime.today().strftime('%Y-%m-%d %H:%M'),emailSt,emailCandi,idSt,status(statusSt),comentarios,"","","","","","","","","","",0],table_range="A:Q")
 def modificarStatus(emailCandi,idSt,emailSt,statusSt,comentariosSt=''):
     client = gspread.authorize(credentials)
     sheet4 = client.open('[EnProceso]EnCliente').worksheet('EnProcesoEnCliente')  # Open the spreadsheet
