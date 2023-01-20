e3b931ee9fe8c332f879c1b95787e760e461ba18
claudio <claudio.x.pc@gmail.com>
2023-01-20 17:25:58 -0300

    se agregan cambios

--- mysql_cc.py
+++ mysql_cc.py
@@ -77,8 +77,11 @@ def encliente(usuario=''):
             a.execute(consulta)
             results = a.fetchall()
             json_data = []
+            source=''
             for result in results:
-                json_data.append([result[0],str(result[12]), result[1], result[2], status(str(result[3])), result[4], result[5],
+                if str(result[12]) is not None:
+                    source=str(result[12])
+                json_data.append([result[0],source, result[1], result[2], status(str(result[3])), result[4], result[5],
                                   result[6], result[7], result[8], result[9], result[10], str(result[11])])
             return json.loads(json.dumps(json_data).encode('utf-8').decode('ascii'))
 
