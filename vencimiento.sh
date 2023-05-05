e8f168d8c38583ababf104ebc19361ccf4927469
alderetebrian <alderetebrian@hotmail.com>
2023-05-05 11:11:05 -0300

    .

---
+++ vencimiento.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+$HOME/.profile;
+cd /root/microservices/sheetToTable
+python3  vencimientoReservas.py
+
